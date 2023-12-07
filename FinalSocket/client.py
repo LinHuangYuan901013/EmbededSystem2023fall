b979a1d7a21783d2067919324ad437b1cc99c683
YUAN <b09901013@ntu.edu.tw>
2023-12-07 22:02:06 +0800

    socket_photo

--- FinalSocket/client.py
+++ FinalSocket/client.py
@@ -2,8 +2,16 @@ import socket
 
 HOST = '192.168.0.16'
 PORT = 8000
-clientMessage = '1234565789'
+photo_array=[1,2,3,4]
 
+def socket_to_server(photo_array):    
+    Message =''
+    for i in photo_array:
+        Message = Message+str(i)+ 'd'
+    return Message
+        
+clientMessage=socket_to_server(photo_array)
+print(clientMessage)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 client.sendall(clientMessage.encode())
